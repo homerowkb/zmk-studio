b2773863caf1de5a9424c8bd429255f08d61e992
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2026-06-12 00:06:10 +0200

    Fix bar shrinking when loading keymaps

--- src/keyboard/Keyboard.tsx
+++ src/keyboard/Keyboard.tsx
@@ -606,7 +606,7 @@ export default function Keyboard() {
 
   return (
     <div className="grid grid-cols-[auto_1fr] grid-rows-[1fr_minmax(10em,auto)] bg-base-300 max-w-full min-w-0 min-h-0">
-      <div className="p-2 flex flex-col gap-2 bg-base-200 row-span-2">
+      <div className="p-2 flex flex-col gap-2 bg-base-200 row-span-2 min-w-44">
         {profileCount > 1 && (
           <ProfilePicker
             profileCount={profileCount}
